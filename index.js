e4c7da94df98a581d6acb9218dcc8b2da821c89c
Nguyen Tien <nguyenmtien465@gmail.com>
2020-08-03 17:18:13 +0700

    ddsadnsakd

---
+++ index.js
@@ -0,0 +1,16 @@
+const hihi=({username,password})=>{
+ return console.log(username,password);
+
+}
+hihi("manhtien","dsd");
+const x = ["hihi","alo", "snda", "gsds", "fsda"];
+x.map((i)=>{
+  i=i+"/html.html"
+})
+for(let i=0;i<x.length;i++){
+  x[i]=x[i]+"dah";
+}
+const y=[...x];
+console.log(y);
+
+
